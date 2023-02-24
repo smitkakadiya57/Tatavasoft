654cf14d2ce9bbd9a9b9412bf43e91af0520bd77
smit kakadiya <smitkakadiya57@gmail.com>
2023-02-24 16:09:05 +0530

    TS DAY 3 task

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,22 @@ import React, { Component } from 'react'
 import Title from '../components/Title'
 
 export class Home extends Component {
+    constructor(){
+        super();
+        this.state={
+            title:"Tatvasoft Project",
+            desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nemo ex officiis fuga aliquam numquam tempora ipsa deserunt ipsam "
+        }
+    }
   render() {
     return (
       <>
       <h1>HOME PAGE</h1>
+      <Title title={this.state.title} desc={this.state.desc} />
       <Title title="Section 1" desc="this is section 1" />
       <Title title="Section 2" desc="this is section 2"/>
       <Title title="Section 3" desc="this is section 3"/>
+      
       </>
     )
   }
