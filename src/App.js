66a74ca2d94f8fd5a330915c7e577c7fb2a7ea9f
smit kakadiya <smitkakadiya57@gmail.com>
2023-02-24 17:00:13 +0530

    TS DAY 5 task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import Home from "./pages/Home"
-import Counter from "./components/Counter"
-const App = () => {
-  return (
-    <>
-    <Counter/>
-    <Home/>
-     </>
-  )
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Counter from "./components/Counter";
+
+import React, { Component } from "react";
+
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      toggle: true,
+    };
+  }
+
+  toggle = () => {
+    this.setState((prev) => ({
+      toggle: !prev.toggle,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <Counter />
+        <br />
+        <br />
+        <br />
+        <button onClick={() => this.toggle()}>Change Page</button>
+        {this.state.toggle ? <Home /> : <About />}
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Title from '../components/Title'
+export class About extends Component {
+  render() {
+    return (
+      <>
+      <Title title="This is About Page" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nemo ex officiis fuga aliquam numquam tempora ipsa deserunt ipsam a!"/>
+      </>
+    )
+  }
+}
+
+export default About
