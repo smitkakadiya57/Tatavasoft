573609a6413ba4743a091613dbed1647d93a951a
smit kakadiya <smitkakadiya57@gmail.com>
2023-02-25 09:45:31 +0530

    TS DAY 7 task

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,12 @@
+import "./title.css";
 
 const Title = ({title,desc,id}) => {
   return (
-    <>
+    <div className="title">
     <h3>ID:{id}</h3>
     <h3>Title:{title}</h3>
     <p>Description:{desc}</p>
-    </>
+    </div>
   )
 }
 
