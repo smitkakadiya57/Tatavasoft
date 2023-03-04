40651de7bd760ece9860bacbc7e8de2e7015361e
smit kakadiya <smitkakadiya57@gmail.com>
2023-03-04 10:07:37 +0530

    TS DAY 8 task

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,14 @@
+import React from "react";
 import "./title.css";
 
-const Title = ({title,desc,id}) => {
+const Title = ({ title, desc, id }) => {
   return (
     <div className="title">
-    <h3>ID:{id}</h3>
-    <h3>Title:{title}</h3>
-    <p>Description:{desc}</p>
+      <h3>ID:{id}</h3>
+      <h3>Title:{title}</h3>
+      <p>Description:{desc}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default React.memo(Title);
